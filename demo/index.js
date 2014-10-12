c5acd90b1b421a631b11e76e79f62fa0eff6dab4
Gil Birman <gbirman@iseatz.com>
2014-10-12 03:10:08 -0500

    unpin, bottomTop (rename)

--- demo/index.js
+++ demo/index.js
@@ -10,57 +10,35 @@ app.controller('appCtrl', function($scope, Tweenable, sparkSetup) {
     console.log("upDown", this);
   };
 
-  this.textSlideOrColor = function (kf, action, o) {
-    kf.element.text(action=='onDown' ? 'color':'slide');
-  };
-
-
 });
 
 app.config(function (sparkFormulas) {
   angular.extend(sparkFormulas, {
-    elementBottomViewportTop: function (element, container, rect, containerRect, offset) {
+
+    // When the bottom of the element hits the top of the viewport
+    bottomTop: function (element, container, rect, containerRect, offset) {
       return ~~(rect.bottom - containerRect.top + offset);
     }
+
   });
 });
 
-app.directive('scopeSvgReveal', function (Tweenable) {
+app.directive('scopeElm', function (Tweenable) {
   return {
     scope: true,
-    priority: 1,
-    controllerAs: 'svgReveal',
-    controller: function ($element) {
-      var svgLength = ~~ $element[0].getTotalLength();
-      var offsetTarget = svgLength;
-      $element[0].style.strokeDasharray = svgLength + ' ' + svgLength;
-
-      var tweenConfig = {
-        from:     { strokeDashoffset: 0  },
-        to:       { strokeDashoffset: offsetTarget },
-        duration: 3000,
-        step: function (state) {
-          $element[0].style.strokeDashoffset = ~~state.strokeDashoffset;
-        }
-      };
-      var tweenable = new Tweenable();
-
-      this.hide = function () {
-        console.log("hide!!");
-        tweenConfig.to.strokeDashoffset = offsetTarget;
-        tweenable.stop();
-        tweenable.tween(tweenConfig);
+    controllerAs: 'elm',
+    controller: function($element) {
+      this.css = function (k, v) {
+        return function () {
+          $element.css(k,v);
+        };
       };
 
-      this.show = function () {
-        console.log("show!!!");
-        tweenConfig.to.strokeDashoffset = 0;
-        tweenable.stop();
-        tweenable.tween(tweenConfig);
+      this.text = function (s) {
+        return function () {
+          $element.text(s);
+        };
       };
-
-
     }
   }
 });
-
